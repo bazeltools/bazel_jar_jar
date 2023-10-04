11b85cdc132f7795d700a25525fe5185cbe087d4
Eugene Yokota <eed3si9n@gmail.com>
2023-10-04 13:18:18 -0400

    Linux?

--- src/test/bazel/jarjar_test.sh
+++ src/test/bazel/jarjar_test.sh
@@ -7,5 +7,6 @@ set -o pipefail
 
 ls "$SCALDING_ARGS__SHADED"
 unzip -l "$SCALDING_ARGS__SHADED" > unzip.txt
+cat unzip.txt
 cat unzip.txt | grep "01-01-2010 00:00"
 cat unzip.txt | grep "foo/Args.class"
